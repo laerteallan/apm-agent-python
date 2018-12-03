ffb2c584ea673804b0081bc655c05e72b1ab82f7
Laerte Allan de Oliveira Araujo <Laerte.araujo@fs.com.br>
2018-12-03 20:24:54 -0200

    Add model tests
    Was added model tests the of slqalchemy_test.py

---
+++ tests/instrumentation/sqlalchemy_test.py
@@ -0,0 +1,25 @@
+import pytest  # isort:skip
+
+pytest.importorskip("sqlalchemy")
+
+import sqlalchemy
+
+
+def test_sqlalchemy_text_clause():
+    pass
+
+
+def test_sqlalchemy_select():
+    pass
+
+
+def test_sqlalchemy_update():
+    pass
+
+
+def test_sqlalchemy_insert():
+    pass
+
+
+def test_error_clause_sql():
+    pass
